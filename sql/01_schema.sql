4bfcf816e7f1318253682932a2f49e542208d662
Abdullah <abdullah-butt-dev@users.noreply.github.com>
2026-06-17 21:04:35 +0500

    feat: create database and import dataset

---
+++ sql/01_schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS sales;
+
+CREATE TABLE sales (
+    row_id INTEGER,
+    order_id VARCHAR(30),
+    order_date DATE,
+    ship_date DATE,
+    ship_mode VARCHAR(50),
+
+    customer_id VARCHAR(30),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+
+    country VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    postal_code VARCHAR(20),
+    region VARCHAR(50),
+
+    product_id VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name TEXT,
+
+    sales NUMERIC(12,2),
+    quantity INTEGER,
+    discount NUMERIC(5,2),
+    profit NUMERIC(12,2)
+);
---
+++ sql/02_import.sql
@@ -0,0 +1,1 @@
+\copy sales FROM '/home/abdullah/Desktop/projects/data-analytics/retail-sales-sql-analysis/data/superstore.csv' WITH (FORMAT CSV, HEADER, ENCODING 'WIN1252')
